1041e04b40e0ccd3ea7616d39598ce463daf65c4
Ruben Castillo <rcastillo@raivtech.com>
2020-08-18 21:10:51 -0600

    Web Scraping

---
+++ script.js
@@ -0,0 +1,52 @@
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+
+async function run() {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    let reviews = []
+    
+    async function getPageData(pageNumber = 1) {
+        await page.goto('http://platzi.com/cursos/html5-css3/opiniones/1/');
+        // await page.screenshot({
+        //     path: 'screenshot.png',
+        //     fullPage: true
+        // })
+        const data = await page.evaluate(() => {
+            const $reviews = document.querySelectorAll('.Review');
+            const $pagination = document.querySelectorAll('.Pagination .Pagination-number')
+            const totalPages = Number($pagination[$pagination.length -1].textContent.trim())
+            const data = [];
+            $reviews.forEach(($review) => {
+                data.push({
+                    username: $review.querySelector('.Review-name').textContent,
+                    taiting: $review.querySelector('.Review-stars .fulled').length,
+                    content: $review.querySelector('.Review-description').textContent.trim(),
+
+                })
+            })
+            
+            return{
+                reviews: data,
+                totalPages
+            }
+        })
+        reviews = [...reviews, ...data.reviews]
+        console.log(`Page ${pageNumber} of ${data.totalPages} completed`)
+        if(pageNumber <= data.totalPages - 50) {
+            getPageData(pageNumber + 1) 
+        } else {
+            fs.writeFile('data.js', `export default ${JSON.stringify(reviews)}`, () => {
+                console.log('Data writed');
+            })
+             await browser.close();
+        }
+        // console.log(data)
+        
+    }
+
+    getPageData()
+   
+}
+
+run()
